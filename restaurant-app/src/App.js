a3bdbc186465417342c06e4e9fe0f7d2db224320
sifat007 <sifat27.ht@gmail.com>
2021-01-25 17:44:47 +0600

    login logout functionality done

--- restaurant-app/src/App.js
+++ restaurant-app/src/App.js
@@ -11,34 +11,55 @@ import RestaurantCreate from './component/RestaurantCreate'
 import RestaurantUpdate from './component/RestaurantUpdate'
 import RestaurantDetail from './component/RestaurantDetail'
 import RestaurantSearch from './component/RestaurantSearch'
+import Login from './component/Login'
+import Logout from './component/Logout'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        
+
         <Route exact path="/">
           <Home></Home>
         </Route>
+
         <Route path="/list">
           <RestaurantsList ></RestaurantsList>
         </Route>
+
         <Route path="/create">
           <RestaurantCreate ></RestaurantCreate>
         </Route>
+
         <Route path="/update/:id"
-        render={props=>(
-<RestaurantUpdate {...props} />
-        )}
+          render={props => (
+            <RestaurantUpdate {...props} />
+          )}
         >
-          
+
         </Route>
+
+
         <Route path="/detail">
           <RestaurantDetail ></RestaurantDetail>
         </Route>
+
         <Route path="/search">
           <RestaurantSearch ></RestaurantSearch>
         </Route>
+
+        <Route path="/login"
+          render={props => (
+            <Login {...props} />
+          )}
+        >
+        </Route>
+
+        <Route path="/logout">
+          <Logout />
+        </Route> 
+
+
       </Router>
     </div>
   );
